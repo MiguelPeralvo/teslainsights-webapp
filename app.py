e1c356e9b41e2367c50ded8a78f52115daab0f0d
MiguelPeralvo <miguel.peralvo@gmail.com>
2018-09-20 18:01:38 +0100

    Starting point for refactoring.

---
+++ app.py
@@ -0,0 +1,116 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State, Event
+import plotly.plotly as py
+from plotly.graph_objs import *
+from scipy.stats import rayleigh
+import flask
+from flask import Flask
+import numpy as np
+import pandas as pd
+import os
+import sqlite3
+import datetime as dt
+
+app = dash.Dash('streaming-teslamonitor-app')
+server = app.server
+
+app.layout = html.Div([
+    html.Div([
+        html.H2("Tesla Monitor Streaming"),
+        html.Img(src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/logo/new-branding/dash-logo-by-plotly-stripe-inverted.png"),
+    ], className='banner'),
+    html.Div([
+        html.Div([
+            html.H3("Tesla Sentiment - Global Trends")
+        ], className='Title'),
+        html.Div([
+            dcc.Graph(id='wind-speed'),
+        ], className='twelve columns wind-speed'),
+        dcc.Interval(id='wind-speed-update', interval=1000, n_intervals=0),
+    ], className='row wind-speed-row')
+], style={'padding': '0px 10px 15px 10px',
+          'marginLeft': 'auto', 'marginRight': 'auto', "width": "900px",
+          'boxShadow': '0px 0px 5px 5px rgba(204,204,204,0.4)'})
+
+
+@app.callback(Output('wind-speed', 'figure'), [Input('wind-speed-update', 'n_intervals')])
+def gen_wind_speed(interval):
+    now = dt.datetime.now()
+    sec = now.second
+    minute = now.minute
+    hour = now.hour
+
+    total_time = (hour * 3600) + (minute * 60) + (sec)
+
+    con = sqlite3.connect("./Data/wind-data.db")
+    df = pd.read_sql_query('SELECT Speed, SpeedError, Direction from Wind where\
+                            rowid > "{}" AND rowid <= "{}";'
+                            .format(total_time-200, total_time), con)
+
+    trace = Scatter(
+        y=df['Speed'],
+        line=Line(
+            color='#42C4F7'
+        ),
+        hoverinfo='skip',
+        error_y=ErrorY(
+            type='data',
+            array=df['SpeedError'],
+            thickness=1.5,
+            width=2,
+            color='#B4E8FC'
+        ),
+        mode='lines'
+    )
+
+    layout = Layout(
+        height=450,
+        xaxis=dict(
+            range=[0, 200],
+            showgrid=False,
+            showline=False,
+            zeroline=False,
+            fixedrange=True,
+            tickvals=[0, 50, 100, 150, 200],
+            ticktext=['200', '150', '100', '50', '0'],
+            title='Time Elapsed (sec)'
+        ),
+        yaxis=dict(
+            range=[min(0, min(df['Speed'])),
+                   max(45, max(df['Speed'])+max(df['SpeedError']))],
+            showline=False,
+            fixedrange=True,
+            zeroline=False,
+            nticks=max(6, round(df['Speed'].iloc[-1]/10))
+        ),
+        margin=Margin(
+            t=45,
+            l=50,
+            r=50
+        )
+    )
+
+    return Figure(data=[trace], layout=layout)
+
+
+external_css = ["https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css",
+                "https://cdn.rawgit.com/plotly/dash-app-stylesheets/737dc4ab11f7a1a8d6b5645d26f69133d97062ae/dash-wind-streaming.css",
+                "https://fonts.googleapis.com/css?family=Raleway:400,400i,700,700i",
+                "https://fonts.googleapis.com/css?family=Product+Sans:400,400i,700,700i"]
+
+
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
+if 'DYNO' in os.environ:
+    app.scripts.append_script({
+        'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
+    })
+
+if __name__ == '__main__':
+    # app.config["CACHE_TYPE"] = "null"
+
+
+    app.run_server(use_reloader=False, debug=True)
