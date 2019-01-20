b281f3a3dee4dc8878a9b616fd69688fd82a55a6
Miguel Peralvo <miguel.peralvo@gmail.com>
2019-01-20 19:40:42 +0000

    Further testing required to match the current timestamp.

--- dash_app/data/data_sentiment.py
+++ dash_app/data/data_sentiment.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime, timedelta
 import sqlite3
 import numpy as np
 import traceback
@@ -17,136 +18,50 @@ def query_tesla_sentiment(url, params):
         # # We revert the dataframe for the visualisation.
         # df = pd.read_json(url).iloc[::-1]
         # # TODO: Calculate volatility
-        # df['volatility'] = df['sentiment_absolute'].rolling(20).std()
-        # df['volatility'] = df['volatility'].bfill()
+        df['volatility'] = df['sentiment_absolute'].rolling(20).std()
+        df['volatility'] = df['volatility'].bfill()
         # # df['volatility'] = np.random.uniform(0, 5, df.shape[0])
     except:
         logger.warning(f'Problem when accessing url {url}: {traceback.format_exc()}')
-        df = pd.DataFrame(columns=['sentiment_type', 'sentiment_seconds_back', 'bin', 'created_at_epoch_ms',
-                               'sentiment_absolute', 'sentiment_normalized',' min_created_at_epoch_ms',
-                               'max_created_at_epoch_ms', 'volatility'
-                               ]
-                      )
+        df = pd.DataFrame(
+            columns=[
+                'sentiment_type', 'sentiment_seconds_back', 'bin', 'created_at_epoch_ms',
+                'sentiment_absolute', 'sentiment_normalized',' min_created_at_epoch_ms',
+                'max_created_at_epoch_ms', 'volatility'
+            ]
+        )
 
     return df
 
 
 def generate_data(params):
-    df = pd.DataFrame(columns=['sentiment_type', 'sentiment_seconds_back', 'bin', 'created_at_epoch_ms',
-                               'sentiment_absolute', 'sentiment_normalized',' min_created_at_epoch_ms',
-                               'max_created_at_epoch_ms', 'volatility'
-                               ]
-                      )
+    map_files = {
+        ('teslamonitor', 400): 'social_teslamonitor_downsample_400_15726.csv',
+        ('teslamonitor', 1200): 'social_teslamonitor_downsample_1200_5663.csv',
+        ('teslamonitor', 21600): 'social_teslamonitor_downsample_21600_324.csv',
+        ('social_external_ensemble', 21600): 'social_external_ensemble_downsample_21600_324.csv',
+        ('news_external_ensemble', 21600): 'news_external_ensemble_downsample_21600_324.csv',
+    }
 
-    return df
+    data_path = '{}/{}'.format('/tmp/data', map_files[(
+        params['sentiment_type'], params['downsample_freq'])
+    ])
 
+    logger.info('Reading data from {}'.format(data_path))
 
-    # delta_ts = datetime.utcnow() - datetime(1970, 1, 1)
-    #
-    # # Bypass potential parameter memoization, but prevent excessive different requests if under attack
-    # utc_now = int(
-    #     (delta_ts.days * 24 * 60 * 60 + (delta_ts.seconds // 10) * 10)) * 1000  # + delta_ts.microseconds / 1000.0)
+    df = pd.read_csv(data_path, header=0).iloc[::-1]
+    epoch_x_days = int(100 * 24 * 60 * 60) * 1000
 
-    # sentiment_type, sentiment_seconds_back, bin, created_at_epoch_ms, sentiment_absolute, sentiment_normalized, min_created_at_epoch_ms, max_created_at_epoch_ms
-    # social_teslamonitor, 43200, 3865191, 1546076256500, 43.0140000, NULL, 1546076216000, 1546076286000
-    # social_teslamonitor, 43200, 3865190, 1546076000658, 45.2756579, NULL, 1546075850000, 1546076179000
+    # We move the historical data x days into the future
+    df = df.assign(
+        created_at_epoch_ms=df['created_at_epoch_ms']+epoch_x_days,
+        min_created_at_epoch_ms=df['min_created_at_epoch_ms'] + epoch_x_days,
+        max_created_at_epoch_ms=df['max_created_at_epoch_ms'] + epoch_x_days
+    )
 
-    # get_tesla_sentiment_quick
-    # params = {
-    #     # 'from_ms_ago': 600000,
-    #     'from_created_epoch_ms': utc_now - 86400000,
-    #     'limit': 250,
-    #     'downsample_freq': 400,
-    #     # 'sample_rate': 0.99,
-    #     'sentiment_type': 'teslamonitor',
-    # }
-    # {
-    #     "created_at_epoch_ms": 1546076286000,
-    #     "max_created_at_epoch_ms": 1546076286000,
-    #     "min_created_at_epoch_ms": 1546076286000,
-    #     "sentiment_absolute": 42.945,
-    #     "sentiment_normalized": null,
-    #     "sentiment_seconds_back": 43200,
-    #     "sentiment_type": "social_teslamonitor"
-    # },
+    now = datetime.utcnow() - datetime(1970, 1, 1)
+    epoch_now = int((now.days * 24 * 60 * 60 + (now.seconds // 10) * 10)) * 1000
 
-
-    # get_tesla_sentiment_slow
-
-    # params = {
-    #     'from_ms_ago': 5760000000,
-    #     # 'from_created_epoch_ms': 1532441907000,
-    #     'limit': 150,
-    #     'downsample_freq': 1200,
-    #     # 'sample_rate': 1.00,
-    #     'sentiment_type': 'teslamonitor',
-    # }
-
-
-
-    # get_tesla_sentiment_historical
-
-    # params = {
-    #     'from_ms_ago': 8640000000,
-    #     # 'from_created_epoch_ms': 1532441907000,
-    #     'limit': 150,
-    #     'downsample_freq': 21600,
-    #     'sentiment_type': 'teslamonitor',
-    #     #'sentiment_type': 'global_external_ensemble',
-    # }
-    # {
-    #     "created_at_epoch_ms": 1546076287000,
-    #     "max_created_at_epoch_ms": 1546076287000,
-    #     "min_created_at_epoch_ms": 1546076287000,
-    #     "sentiment_absolute": 57.537,
-    #     "sentiment_normalized": null,
-    #     "sentiment_seconds_back": 43200,
-    #     "sentiment_type": "global_external_ensemble"
-    # },
-
-
-
-    # get_external_social_sentiment_historical
-
-
-    #     params = {
-    #         'from_ms_ago': 8640000000,
-    #         # 'from_created_epoch_ms': 1532441907000,
-    #         'limit': 150,
-    #         'downsample_freq': 21600,
-    #         'sentiment_type': 'social_external_ensemble',
-    #
-    #     }
-    # {
-    #     "created_at_epoch_ms": 1546076287000,
-    #     "max_created_at_epoch_ms": 1546076287000,
-    #     "min_created_at_epoch_ms": 1546076287000,
-    #     "sentiment_absolute": 35.0,
-    #     "sentiment_normalized": null,
-    #     "sentiment_seconds_back": 43200,
-    #     "sentiment_type": "social_external_ensemble"
-    # },
-
-
-
-    # get_external_news_sentiment_historical
-
-    # params = {
-    #     'from_ms_ago': 8640000000,
-    #     # 'from_created_epoch_ms': 1532441907000,
-    #     'limit': 150,
-    #     'downsample_freq': 21600,
-    #     'sentiment_type': 'news_external_ensemble',
-    #
-    # }
-    # {
-    #     "created_at_epoch_ms": 1546076287000,
-    #     "max_created_at_epoch_ms": 1546076287000,
-    #     "min_created_at_epoch_ms": 1546076287000,
-    #     "sentiment_absolute": 80.22,
-    #     "sentiment_normalized": null,
-    #     "sentiment_seconds_back": 43200,
-    #     "sentiment_type": "news_external_ensemble"
-    # },
-
-
+    logger.info('Selecting data older than {}'.format(epoch_now))
+    df = df.loc[df['created_at_epoch_ms'] <= epoch_now]
+    return df[:params['limit']]
